a96756ecb0d280a4d3c88cada689641fb9ea1126
Garuda-99 <165791347+Garuda-99@users.noreply.github.com>
2026-06-29 19:12:33 +0530

    Event Loop In NodeJs

---
+++ nodejsConcepts/eventLoop.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const crypto = require("crypto");
+console.log("1.Start");
+setTimeOut(() => {
+  console.log("2.0s callback");
+}, 0);
+setTimeOut(() => {
+  console.log("3.2nd 0s callback");
+}, 0);
+setImmediate(() => {
+  console.log("4.immediate callback");
+});
+Promise.resolve().then(() => {
+  console.log("5.promise callback(microtask)");
+});
+process.nextTick(() => {
+  console.log("6.process.nextTick callback (microtask");
+});
+fs.readFile(__filename, () => {
+  console.log("7.fs callback (i/o call back)");
+});
+crypto.pbkdf2("secret", "salt", 10000, 64, "sha512", (err, key) => {
+  if (err) throw err;
+  console.log("8.pbkdf2 callback");
+});
+console.log("end");
